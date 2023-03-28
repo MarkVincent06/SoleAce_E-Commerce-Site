c8cb203b2bd475c8c382651bd8a06c19c3bb7441
Blueboltz <vincentmariscotescleofe@gmail.com>
2023-03-28 20:56:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SoleAce_E-Commerce_Site
+ A shoe ecommerce website project in E-Commerce Technology
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- CSS LINK -->
+   <link rel="stylesheet" href="./styles/index.css">
+
+   <!-- GOOGLE FONTS LINK -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@300;400;700;900&display=swap" rel="stylesheet">
+   
+   <title>Dashboard - SoleAce</title>
+</head>
+<body>
+   <!-- START OF HEADER -->
+   <header>
+      
+   </header>
+   <!-- END  OF HEADER -->
+</body>
+</html>
