36da0b520d7b5fde1f6b3ecaf8aa70f95490b514
Blueboltz <vincentmariscotescleofe@gmail.com>
2023-03-29 00:03:08 +0800

    added navigation and hero sections

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,14 +13,44 @@
    <link rel="preconnect" href="https://fonts.googleapis.com">
    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
    <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@300;400;700;900&display=swap" rel="stylesheet">
-   
+
    <title>Dashboard - SoleAce</title>
 </head>
+
 <body>
    <!-- START OF HEADER -->
-   <header>
-      
-   </header>
+   <nav>
+      <a href="#" class="business-logo-container">
+         <img class="business-logo" src="./img/business-logo.png" alt="An image of a business logo">
+      </a>
+
+      <ul class="categories">
+         <li><a href="#">New & Featured</a></li>
+         <li><a href="#">Men</a></li>
+         <li><a href="#">Women</a></li>
+         <li><a href="#">Kids</a>
+         </li>
+      </ul>
+
+      <a class="account-link" href="">
+         <h3>My Account</h3>
+      </a>
+
+      <!-- WILL CHANGE THIS TO IMAGE TO ICON LATER -->
+      <a href="" class="shopping-cart-link">
+         <img src="./img/icons/shopping-cart.png" alt="An icon of a shopping cart">
+      </a>
+   </nav>
    <!-- END  OF HEADER -->
+
+   <main>
+      <!-- START OF HERO -->
+      <section class="hero">
+         <h1 class="hero--slogan">Unleash Your Sole<br> Power With<br> <span>SoleAce</span></h1>
+         <img class="hero--shoe-item" src="./img/hero-shoe-item.png" alt="A picture of a big shoe">
+      </section>
+      <!-- END OF HERO -->
+   </main>
 </body>
+
 </html>
